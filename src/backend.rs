273b230a1275e346d87a94cddc521086457684c3
meee-low <my_milo_m@hotmail.com>
2023-09-13 19:04:45 -0300

    pseudocode

--- src/backend.rs
+++ src/backend.rs
@@ -1,1 +1,15 @@
-fn main() {}
+// Use crates
+
+fn main() {
+    // If database doesn't exist, create it based on the schema and populate the database with the starting values from the TOML
+
+
+    //
+}
+
+
+// Tables/Schema
+
+// Structs
+
+// APIs
