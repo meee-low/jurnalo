8bedf2bbb898287542650ecc298783f6f500492a
meee-low <my_milo_m@hotmail.com>
2023-09-14 10:52:22 -0300

    Changed id's to INTEGER PK, as recommended by SQLite
    SQLite automatically uses INTEGER PK columns as an alias to rowid.
    Autoincrement is unnecessary and more expensive computationally.
    
    No API change, only internal.
    
    https://www.sqlitetutorial.net/sqlite-autoincrement/

--- migrations/2023-09-13-230129_create_tables/up.sql
+++ migrations/2023-09-13-230129_create_tables/up.sql
@@ -1,13 +1,13 @@
 -- Your SQL goes here
 
 CREATE TABLE categories (
-	"id"	INTEGER NOT NULL UNIQUE,
+	"id"	INTEGER,
 	"label"	TEXT NOT NULL UNIQUE,
 	"prompt"	TEXT NOT NULL UNIQUE,
 	"category_type"	INTEGER NOT NULL DEFAULT 1,
 	"disabled_bool" INTEGER NOT NULL DEFAULT 0,
 	"extra_info" TEXT,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 	FOREIGN KEY("category_type")
 		REFERENCES "category_types" ("id")
 		ON DELETE CASCADE
@@ -15,10 +15,10 @@ CREATE TABLE categories (
 );
 
 CREATE TABLE "category_option" (
-	"id"	INTEGER NOT NULL UNIQUE,
+	"id"	INTEGER,
 	"category_id"	INTEGER NOT NULL,
 	"option_id"	INTEGER NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 	FOREIGN KEY ("category_id")
 		REFERENCES "categories" ("id")
 		ON DELETE CASCADE
@@ -30,12 +30,12 @@ CREATE TABLE "category_option" (
 );
 
 CREATE TABLE "entries" (
-	"id"	INTEGER NOT NULL UNIQUE,
+	"id"	INTEGER,
 	"timestamp"	TIMESTAMP NOT NULL,
 	"category"	INTEGER,
 	"value"	INTEGER,
 	"details"	TEXT,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 	FOREIGN KEY ("category")
 		REFERENCES "categories" ("id")
 		ON DELETE CASCADE
@@ -47,31 +47,31 @@ CREATE TABLE "entries" (
 );
 
 CREATE TABLE "options" (
-	"id"	INTEGER NOT NULL UNIQUE,
+	"id"	INTEGER,
 	"label"	TEXT NOT NULL UNIQUE,
 	"shortcut"	TEXT NOT NULL UNIQUE,
 	"disabled_bool" INTEGER NOT NULL DEFAULT 0,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 );
 
 CREATE TABLE "category_types" (
-	"id" INTEGER NOT NULL UNIQUE,
+	"id" INTEGER,
 	"label" TEXT NOT NULL UNIQUE,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 );
 
 CREATE TABLE "batteries" (
-	"id" INTEGER NOT NULL UNIQUE,
+	"id" INTEGER,
 	"label" TEXT NOT NULL UNIQUE,
 	"command" TEXT UNIQUE,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 );
 
 CREATE TABLE "batteries_to_categories" (
-	"id" INTEGER NOT NULL UNIQUE,
+	"id" INTEGER,
 	"battery_id" INTEGER NOT NULL,
 	"category_id" INTEGER NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 	FOREIGN KEY("battery_id")
 		REFERENCES "batteries" ("id")
 		ON DELETE CASCADE
